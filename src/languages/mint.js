53e5d191f88391b652bd9b970e16316cb72e122e
Sijawusz Pur Rahnama <sija@sija.pl>
2021-07-25 14:46:12 +0200

    Oops, use lookahead instead

--- src/languages/mint.js
+++ src/languages/mint.js
@@ -79,7 +79,7 @@ export default function (hljs) {
         contains: MINT_CONTAINS,
       },
       {
-        begin: '</?\\w+',
+        begin: '</?(?=\\w+)',
         end: '/?>',
         className: 'tag',
         contains: [
